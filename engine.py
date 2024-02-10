8f9ec0db08fa17f32776310b37d29484a7ea2b2b
Saegl <saegl@protonmail.com>
2024-02-10 15:37:22 +0600

    Create testing framework, to test against builtin sqlite3

--- engine.py
+++ engine.py
@@ -1,28 +1,54 @@
 import parser
 
 
+class Table:
+    columns: list[str]
+    data: list[list[str]]
+    
+    def __init__(self, columns: list[str]):
+        self.columns = columns
+        self.data = []
+    
+    def insert_row(self, row: list[str]):
+        self.data.append(row)
+
+
 class Engine:
+    tables: dict[str, Table]
+
     def __init__(self):
-        self.tabledata = {}
-        self.tablecols = {}
+        self.tables = {}
 
     def createstmt(self, stmt: parser.CreateStmt):
-        self.tabledata[stmt.tablename] = []
-        self.tablecols[stmt.tablename] = []
-        for coldef in stmt.columndefs:
-            self.tablecols[stmt.tablename].append((coldef.column_name, coldef.column_name))
+        table = Table([cd.column_name for cd in stmt.columndefs])
+        self.tables[stmt.tablename] = table
 
     def insertstmt(self, stmt: parser.InsertStmt):
+        table = self.tables[stmt.tablename]
         for row in stmt.values:
-            self.tabledata[stmt.tablename].append(row.exprs)
+            table.insert_row(row.exprs)
 
     def selectstmt(self, stmt: parser.SelectStmt):
-        if stmt.tablename not in self.tabledata:
+        if stmt.tablename not in self.tables:
             print(f"OperationalError (SQLITE_ERROR): no such table: {stmt.tablename}")
             return
+        
+        output = []
+        for row in self.tables[stmt.tablename].data:
+            output.append(tuple(row))
+        return output
 
-        for row in self.tabledata[stmt.tablename]:
-            print(row)
+    def execute(self, cmd: str) -> list:
+        cmd = cmd + ';'
+        for stmt in parser.parse(cmd):
+            stmtname = stmt.__class__.__name__.lower()
+            method = getattr(self, stmtname)
+            output = method(stmt)
+            if output is None:
+                return []
+            else:
+                return output
+        return []
 
     def eval(self, line):
         for stmt in parser.parse(line):
---
+++ test_engine.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+from engine import Engine
+
+
+class SqliteWrapper:
+    """
+    I am not interested in implementing Transaction control
+    (manual commit after each insert)
+    So this wrapper commits on each executes, useful for testing,
+    but this is not proper use of `sqlite3`
+    """
+    def __init__(self):
+        self.con = sqlite3.connect(":memory:")
+        self.cur = self.con.cursor()
+
+    def execute(self, text: str):
+        res = self.cur.execute(text)
+        self.con.commit()
+        return res.fetchall()
+
+
+class SameOutput:
+    def __init__(self):
+        self.sw = SqliteWrapper()
+        self.e = Engine()
+
+    def same(self, cmd: str):
+        assert self.sw.execute(cmd) == self.e.execute(cmd)
+
+
+def test1():
+    sm = SameOutput()
+    sm.same("CREATE TABLE user(firstname TEXT, secondname TEXT)")
+    sm.same("INSERT INTO user VALUES ('alisher', 'zhuban'), ('john', 'doe')")
+    sm.same("SELECT * FROM user")
+    sm.same("SELECT firstname, secondname FROM user")
+
